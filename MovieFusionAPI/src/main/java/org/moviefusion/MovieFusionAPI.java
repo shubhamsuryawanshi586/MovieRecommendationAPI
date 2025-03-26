28f5c51e3dde33f99358f6bff52b71bfc8880e54
shubhamsuryawanshi586 <shubhamsuryawanshi586@gmail.com>
2025-03-27 02:26:09 +0530

    Main method class name changed

--- MovieFusionAPI/src/main/java/org/moviefusion/MovieFusionAPI.java
+++ MovieFusionAPI/src/main/java/org/moviefusion/MovieFusionAPI.java
@@ -5,13 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieFusionApiApplication {
+public class MovieFusionAPI { // class name changed
 	private static boolean swaggerOpened = true;
 	public static void main(String[] args) {
 		
 		if(swaggerOpened)
 		{
-			SpringApplication.run(MovieFusionApiApplication.class, args);
+			SpringApplication.run(MovieFusionAPI.class, args);
 			SwaggerConfigs.openSwaggerUI();
 			swaggerOpened = false;
 		}
